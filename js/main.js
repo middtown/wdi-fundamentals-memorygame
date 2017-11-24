615a825c73e6465f29a41c75d23ba84a1a93668a
Aaron Middleton <middtown@Aarons-MacBook-Air.local>
2017-11-24 07:15:58 -0700

    updated with cardFlip and checkForMatch functions

--- js/main.js
+++ js/main.js
@@ -1,21 +1,20 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
+var checkForMatch = function(){
+ 	if(cardsInPlay.length === 2)
+    if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
-}else{
+	}else{
 	alert("Please try again.");
+	}
 }
 
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    checkForMatch();
+}
 
-
-//console.log("User Flipped " + cardsInPlay);
-
-
-/* if (cardsInPlay.length === 2){
-	alert(" two cards played!");
-} else */
+flipCard(0);
+flipCard(2);
