e8cc57f341af6902c863903f0d1bb02213543931
Aaron Middleton <middtown@Aarons-MacBook-Air.local>
2017-11-24 11:35:57 -0700

    created game logic for card array

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png" 
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  }
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
- 	if(cardsInPlay.length === 2)
+  if(cardsInPlay.length === 2)
     if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 	}else{
@@ -11,10 +33,20 @@ var checkForMatch = function(){
 }
 
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
-    checkForMatch();
+	console.log("User flipped " + cards[cardId].rank);
+    cardsInPlay.push(cards[cardId].rank);
+    console.log(cards[cardId].cardImage);
+    console.log(cards[cardId].suit);
+      checkForMatch();
 }
 
 flipCard(0);
 flipCard(2);
+
+
+
+
+
+
+
+
